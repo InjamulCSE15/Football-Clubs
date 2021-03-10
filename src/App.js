df98a4668a4293bdff5d521cb37840e51bac25e9
InjamulCSE15 <yeasin.mohammod018@gmail.com>
2021-03-10 11:43:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 function App() {
+  const buttonStyle = {
+    color: 'white',
+    backgroundColor: 'blue',
+    height: '40px',
+    borderRadius: '5px',
+    margin: 'auto',
+    padding: 'auto'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>This is font awesome</h1>
+        <button style={buttonStyle}><FontAwesomeIcon icon={faCoffee} />Explore Here</button>
     </div>
   );
 }
